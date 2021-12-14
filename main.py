449f7c9386227dfccbadb56a67ef1d8468336a09
Yoieh <johan.nilsson@nerdsofsweden.se>
2021-12-15 00:23:43 +0100

    just a simpel first print of the data from CLItxt

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-# hello world
-print("Hello World!")
+from pyrover import CLItxt
+
+
+MAX_GRID_SIZE = 100
+
+
+class Main():
+    def __init__(self):
+        self.cli = CLItxt(MAX_GRID_SIZE)
+
+    def run(self):
+        print(self.cli.grid_size)
+        print(self.cli.start_pos)
+        print(self.cli.commands)
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
